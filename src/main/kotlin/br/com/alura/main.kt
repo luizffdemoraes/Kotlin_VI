72c51feae58e9639fec2ca9709066fe77663e73d
Luiz Moraes <lffm1994@gmail.com>
2021-06-06 19:33:44 -0300

    Modulo 1 - Tipos de coleção

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -1,13 +1,44 @@
 package br.com.alura
 
 fun main() {
-    /*
-        Criação de list List<String>
-        O list e o set compartilham de Collection e Iterable elas implementam de seus comportamentos
-        -> Collection<String> / Iterable<String>
-        Ao utilizar os genericos temos restrição de comportamento
-     */
-    val nomes: Collection<String> = setOf(
+//    testaColecao()
+    val banco = BancoDeNomes()
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+
+
+}
+
+class BancoDeNomes{
+
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String){
+        dados.add(nome)
+    }
+
+    // Ao executar o código a gente vai perceber que os dados serão mantidos.
+    // Se a gente salvou alguma vez em algum lugar eles serão mantidos
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+}
+
+
+/*
+       Criação de list List<String>
+       O list e o set compartilham de Collection e Iterable elas implementam de seus comportamentos
+       -> Collection<String> / Iterable<String>
+       Ao utilizar os genericos temos restrição de comportamento
+       Há dois tipos de coleções read-only e mutable
+       read-only: não permite mudar os valores
+       mutable: permite a leitura e modificações
+       Iterable ou MutableIterable tem o comportamento mais restrito
+ */
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
         "Alex",
         "Fran",
         "Gui",
@@ -15,7 +46,7 @@ fun main() {
         "Ana"
     )
 
-    for(nome in nomes.iterator()){
+    for (nome in nomes.iterator()) {
         println(nome)
     }
 
