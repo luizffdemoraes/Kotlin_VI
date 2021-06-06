d71e81b8ad25373ee007f08243346ee2deb05228
Luiz Moraes <lffm1994@gmail.com>
2021-06-06 20:34:18 -0300

    Modulo 2 - Utilizando o Set

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -2,13 +2,20 @@ package br.com.alura
 
 fun main() {
 //    testaCopia()
-    val assistiramCursoAndroid: List<String> = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin: List<String>  = listOf("Alex", "Paulo", "Maria")
+    //List e Set estão na mesma hierarquia não herdam um do outro
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String>  = setOf("Alex", "Paulo", "Maria")
 //    val assistiramAmbos: MutableList<String> = mutableListOf<String>()
-    val assistiramAmbos: List<String> = assistiramCursoAndroid + assistiramCursoKotlin
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos:MutableSet<String> = mutableSetOf<String>()
 //    assistiramAmbos.addAll(assistiramCursoAndroid)
 //    assistiramAmbos.addAll(assistiramCursoKotlin)
     //Função "distinct" - que o objetivo dele é realmente devolver
     // uma nova lista que vai ter apenas os elementos distintos.
-    println(assistiramAmbos.distinct())
+//    println(assistiramAmbos.distinct())
+//    println(assistiramAmbos[0])
+    //O set sempre vai ter um conjunto sem repetição de elementos
+    assistiramAmbos.addAll(assistiramCursoAndroid + assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
 }
