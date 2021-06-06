d127bd6bb168c2e8f197a12ee713ada81f405796
Luiz Moraes <lffm1994@gmail.com>
2021-06-06 20:53:19 -0300

    Modulo 2 - Comportamentos do Set

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -18,4 +18,39 @@ fun main() {
     assistiramAmbos.addAll(assistiramCursoAndroid + assistiramCursoKotlin)
     assistiramAmbos.add("Ana")
     println(assistiramAmbos)
+
+    /*
+     Caracteristica do Union
+     É uma implementação mais genérica,não só focada na parte do set,
+     pode receber qualquer iterable,
+     e aqui na união a gente vê que é diferente. Essa é uma das coisas.
+     Outro detalhe que acaba sendo diferente é que esse union vai ter uma característica
+     de implementação que a gente chama de infix no Kotlin podemos utilizar o ponto e sem o ponto.
+     println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+     println(assistiramCursoAndroid union(assistiramCursoKotlin))
+     println(assistiramCursoAndroid union assistiramCursoKotlin)
+     */
+
+    //São similares
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid union(assistiramCursoKotlin))
+
+    //São similares o subtract e semelhante ao union é um infix
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
+
+    /*
+        Intersecção - Em teoria dos conjuntos, a interseção ou intersecção,
+        é um conjunto de elementos que, simultaneamente,
+        pertencem a dois ou mais conjuntos,
+     */
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList: MutableList<String> = assistiramAmbos.toMutableList()
+
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet()) //coloca o comportamento do set podemos converter para o set
+
 }
